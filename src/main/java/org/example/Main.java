1eab9ea9ee89eb7badc06417752864b71cac8822
Memona1234 <115113221+Memona1234@users.noreply.github.com>
2022-10-07 15:01:38 -0500

    done
    asfd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+class Main {
+
+    public static void main(String[] args) {
+        //size of triangle
+        int size = 4;
+
+        for (int i = 0; i < size; i++) {
+
+            for(int j = 0; j <= i; j++ ) {
+
+                System.out.print("*"); }
+
+            System.out.println();
+
+        }
+
+    }
+}
